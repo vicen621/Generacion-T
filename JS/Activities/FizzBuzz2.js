7ff57a21823b8510e7edf8fd8756cf6b826faeee
vicen621 <71749981+vicen621@users.noreply.github.com>
2022-10-05 17:40:18 -0300

    Finished FizzBuzz2

--- JS/Activities/FizzBuzz2.js
+++ JS/Activities/FizzBuzz2.js
@@ -1,14 +1,15 @@
-function fizzBuzz2(fizz, fizzNum, buzz, buzzNum, maxNum) {
+function fizzBuzz2(fizz, buzz, fizzNum, buzzNum, maxNum) {
     for (i = 1; i < maxNum + 1; i++) {
         let str = "";
-        
+
         if (i % fizzNum === 0)
-            str += fizz
+            str += fizz;
+
         if (i % buzzNum === 0)
-            str += buzz
-        
-        console.log(str === "" ? i : str); 
+            str += buzz;
+
+        console.log(str === "" ? i : str);
     }
 }
 
-fizzBuzz2("Vicente", 5, " Viloni", 7, 100);
+fizzBuzz2("Vicente", "Viloni", 5, 7, 100);
