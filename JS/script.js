68e15dfce6775251bbfd641ebb629349da027fe7
vicen621 <71749981+vicen621@users.noreply.github.com>
2022-10-06 16:24:37 -0300

    Added some excercises of class 3

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+//Doubles
+/* let numbers = [3, 7, 13, 99];
+let doubles = numbers.map(n => n*2);
+
+console.log(numbers)
+console.log(doubles) */
+
+
+//Exclamation Phrases
+/* let phrases = ["Tonto", "Pelo", "Cabeza", "Larreta", "Pelado Puto"]
+
+let exclamationPhrases = phrases.map(p => {
+    return "¡" + p + "!"
+}) */
+
+// Reduce - 1
+/* let numbers = [6, 1, 34, 94, 3, 17]
+
+const mul = numbers.reduce((before, next )=> before * next);
+
+console.log(mul); */
