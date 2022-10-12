2ba5173c9775e2963c2257c9465c9c6a96fa5b61
vicen621 <71749981+vicen621@users.noreply.github.com>
2022-10-12 17:23:06 -0300

    Hice mas ejercicios de js

--- JS/script.js
+++ JS/script.js
@@ -19,3 +19,64 @@ let exclamationPhrases = phrases.map(p => {
 const mul = numbers.reduce((before, next )=> before * next);
 
 console.log(mul); */
+
+// Characters Chain
+/* function charactersChain(text) {
+    text = text.replace("?", "");
+    text = text.replace("!", "");
+    let array = text.split(" ");
+    
+    console.log("El texto contiene " + array.length + " palabras")
+    console.log("La primera palabra es " + array[0]);
+    console.log("La ultima palabra es " + array[array.length - 1]);
+} */
+
+//Promedio de estudiantes
+/*let students = [
+    {
+        name: "Jose",
+        state: "approved",
+        notes: [8, 7, 6, 4, 10]
+    },
+    {
+        name: "Azul",
+        state: "approved",
+        notes: [8, 7, 6, 4, 10]
+    },
+    {
+        name: "Pablo",
+        state: "disapproved",
+        notes: [3, 6, 1, 4, 10]
+    },
+    {
+        name: "Vicente",
+        state: "disapproved",
+        notes: [3, 6, 1, 4, 10]
+    },
+    {
+        name: "Violeta",
+        state: "approved",
+        notes: [8, 7, 6, 4, 10]
+    },
+    {
+        name: "Clara",
+        state: "disapproved",
+        notes: [3, 6, 1, 4, 10]
+    }
+]
+
+function average(students, state) {
+    let returnArray = [];
+    for (i = 0; i < students.length; i++) {
+        student = students[i];
+        let notesNum = student.notes;
+        console.log(notesNum)
+        student.average = Math.round(student.notes.reduce((prev, curr) => prev + curr) / notesNum.length);
+        if (student.state === state) {
+            returnArray.push(student);
+        }
+    }
+
+    return returnArray;
+}*/
+
