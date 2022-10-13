7eecea38a10cce2dd922fb993727b7a1f23d043c
vicen621 <71749981+vicen621@users.noreply.github.com>
2022-10-13 16:55:45 -0300

    Finished Form&input

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,22 @@
-/* let source = document.querySelector("#source")
-let selector = document.querySelector("#destination")
+let name;
+let mail;
+let sexo;
+let medad = document.getElementById("medad");
+let edad;
+let argentino;
+let submit = document.querySelector('#submit');
 
-let sourceContent = source.textContent;
+submit.addEventListener('click', () => {
+    name = document.getElementById('fname').value;
+    mail = document.getElementById('fmail').value;
+    sexo = document.querySelector('input[name="sex"]:checked').value;
+    edad = medad.options[medad.selectedIndex].text;
+    argentino = document.getElementById('argentino').checked;
 
-source.textContent = selector.textContent;
-selector.textContent = sourceContent; */
-
+    alert("Lee la consola")
+    console.log(name)
+    console.log(mail)
+    console.log(sexo)
+    console.log(edad)
+    console.log(argentino)
+})
