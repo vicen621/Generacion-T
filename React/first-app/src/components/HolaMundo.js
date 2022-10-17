2ef522bf6353884260bcad5c87958d209f57fc33
vicen621 <71749981+vicen621@users.noreply.github.com>
2022-10-17 17:12:52 -0300

    Start using React

---
+++ React/first-app/src/components/HolaMundo.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const HolaMundo = props => {
+    return(
+        <div><p>Hola {props.name}, bienvenido al mundo de React!</p></div>
+    );
+}
+
+export default HolaMundo;
